155e224890181f3a3b2a40688f6018a74fccc2a5
Vishnu Saini <vishnu@Vishnus-MacBook-Pro.local>
2020-12-29 20:35:34 +0530

    first commit

---
+++ src/com/vishnu/thread1/termination/TerminationWIthIntruptableCode.java
@@ -0,0 +1,23 @@
+package com.vishnu.thread1.termination;
+
+public class TerminationWIthIntruptableCode {
+	public static void main(String[] args) {
+		Thread thread = new Thread(new BlockingTask());
+
+		thread.start();
+		thread.interrupt();
+	}
+
+	private static class BlockingTask implements Runnable {
+
+		@Override
+		public void run() {
+			// do things
+			try {
+				Thread.sleep(500000);
+			} catch (InterruptedException e) {
+				System.out.println("Existing blocking thread");
+			}
+		}
+	}
+}
